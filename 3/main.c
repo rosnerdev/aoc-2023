ca0f9add8778d2d3363f9df95837461fa6553013
rosnerdev <83503879+rosnerdev@users.noreply.github.com>
2023-12-29 22:45:25 +0200

    finally finished day 3!

--- 3/main.c
+++ 3/main.c
@@ -3,13 +3,137 @@
 #include "string.h"
 #include "ctype.h"
 
+char (*get_mat(const char*))[140];
+int (*get_coords(char (*mat)[140]))[2];
+int get_gears(char (*mat)[140]);
+
 void part1();
-/*void part2();*/
+void part2();
 
 int main() {
     part1();
-    /*part2();*/
+    part2();
     return 0;
+}
+
+void part2() {
+    double sum = 0;
+    double ratio = 1;
+    int found = 0;
+    char curr_num[10];
+    int num_i = 0;
+    int track = 0;
+    int (*nums)[4] = calloc(1, sizeof(int[4]));
+    char (*mat)[140] = get_mat("input.txt");
+    int (*coords)[2] = get_coords(mat);
+    int gears_num = get_gears(mat);
+
+    int i, j;
+    for (i = 0; i < 140; i++) {
+        for (j = 0; j < 140; j++) {
+            if (isdigit(mat[i][j]) && j != 139) {
+                curr_num[num_i] = mat[i][j];
+                num_i++;
+            } else {
+                if (isdigit(mat[i][j]) && j == 139) {
+                    curr_num[num_i] = mat[i][j];
+                    j++;
+                }
+
+                nums = realloc(nums, sizeof(int[4]) * (track + 1));
+                nums[track][0] = j - strlen(curr_num);
+                nums[track][1] = j;
+                nums[track][2] = atoi(curr_num);
+                nums[track][3] = i;
+                track++;
+                num_i = 0;
+                memset(curr_num, '\0', sizeof(curr_num));
+            }
+        }
+    }
+
+    for (i = 0; i < gears_num; i++) {
+        for (j = 0; j < track + 1; j++) {
+            int x;
+            for (x = nums[j][0]; x < nums[j][1]; x++) {
+                if ((coords[i][0] == x - 1 || coords[i][0] == x || coords[i][0] == x + 1) && (coords[i][1] == nums[j][3] - 1 || coords[i][1] == nums[j][3] || coords[i][1] == nums[j][3] + 1)) {
+                    found++;
+                    ratio *= nums[j][2];
+                    break;
+                }
+            }
+        }
+
+        if (found == 2) {
+            sum += ratio;
+        }
+        found = 0;
+        ratio = 1;
+    }
+
+    free(nums);
+
+    printf("Part 2: %.f\n", sum);
+}
+
+int get_gears(char (*mat)[140]) {
+    int gears = 0;
+    int i, j;
+    for (i = 0; i < 140; i++) {
+        for (j = 0; j < 140; j++) {
+            if (mat[i][j] == '*') {
+                gears++;
+            }
+        }
+    }
+
+    return gears;
+}
+
+int (*get_coords(char (*mat)[140]))[2] {
+    int (*coords)[2] = calloc(1, sizeof(int[2]));
+    int track = 0;
+    int i, j;
+
+    if (coords == NULL) {
+        printf("Memory allocation failed.");
+        exit(1);
+    }
+
+    for (i = 0; i < 140; i++) {
+        for (j = 0; j < 140; j++) {
+            if (mat[i][j] == '*') {
+                coords = realloc(coords, sizeof(int[2]) * (track + 1));
+                coords[track][0] = j;
+                coords[track][1] = i;
+                track++;
+            }
+        }
+    }
+
+    return coords;
+}
+
+char (*get_mat(const char* filename))[140] {
+    char (*mat)[140] = calloc(140, sizeof(char[140]));
+    char c;
+    int i = 0, j = 0;
+    FILE* file;
+    file = fopen(filename, "r");
+
+    while ((c = getc(file)) != EOF) {
+        if (c != '\n') {
+            mat[i][j] = c;
+            j++;
+        } else {
+            i += 1;
+            j = 0;
+        }
+    }
+
+    fclose(file);
+
+    return mat;
 }
 
 int special(char x) {
@@ -88,7 +212,7 @@ void part1() {
 	
 	for (i = 0; i < 140; i++) {
         for (j = 0; j < 140; j++) {	
-            if (isdigit(mat[i][j])) {
+            if (isdigit(mat[i][j]) && j != 139) {
                 curr_num[num_i] = mat[i][j];
                 num_i++;
             } else {
